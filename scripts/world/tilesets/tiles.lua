d4a1e81e673243bc5fd9a4ff87006a3bbd47b8c0
THat W. A. Guy <milo.skewes@outlook.com>
2024-10-15 14:25:01 -0500

    Add files via upload

---
+++ scripts/world/tilesets/tiles.lua
@@ -0,0 +1,31 @@
+return {
+  version = "1.11",
+  luaversion = "5.1",
+  tiledversion = "1.11.0",
+  name = "tiles",
+  class = "",
+  tilewidth = 20,
+  tileheight = 20,
+  spacing = 0,
+  margin = 0,
+  columns = 16,
+  image = "../../../assets/sprites/tilesets/tiles.png",
+  imagewidth = 320,
+  imageheight = 160,
+  objectalignment = "unspecified",
+  tilerendersize = "grid",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 20,
+    height = 20
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 128,
+  tiles = {}
+}
---
+++ scripts/world/tilesets/tiles.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.0" name="tiles" tilewidth="20" tileheight="20" tilecount="128" columns="16" tilerendersize="grid">
+ <editorsettings>
+  <export target="tiles.lua" format="lua"/>
+ </editorsettings>
+ <image source="../../../assets/sprites/tilesets/tiles.png" width="320" height="160"/>
+</tileset>

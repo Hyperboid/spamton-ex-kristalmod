a3156cc944562f26acbde97a9a12ef71badda4e8
hyperboid <hyperboid@nullanoid.net>
2024-10-15 17:50:46 -0400

    Basic flying head attack

--- scripts/battle/waves/basic.lua
+++ scripts/battle/waves/basic.lua
@@ -1,20 +1,29 @@
+---@class BasicWave : Wave
 local Basic, super = Class(Wave)
 
 function Basic:onStart()
-    -- Every 0.33 seconds...
-    self.timer:every(1/3, function()
+    -- Every 0.66 seconds...
+    self.timer:every(2/3, function()
         -- Our X position is offscreen, to the right
-        local x = SCREEN_WIDTH + 20
+        local x = Utils.pick({SCREEN_WIDTH + 20, -20})
         -- Get a random Y position between the top and the bottom of the arena
-        local y = Utils.random(Game.battle.arena.top, Game.battle.arena.bottom)
+        local y = Utils.pick({Game.battle.arena.top + 10, Game.battle.arena.y, Game.battle.arena.bottom - 10})
 
         -- Spawn smallbullet going left with speed 8 (see scripts/battle/bullets/smallbullet.lua)
-        local bullet = self:spawnBullet("flyinghead", x, y, math.rad(180), 0)
-        bullet.target_x = Game.battle.arena.x
-
-        -- Dont remove the bullet offscreen, because we spawn it offscreen
-        bullet.remove_offscreen = false
+        self.timer:script(function(wait)
+            for i=1,4 do
+                local bullet = self:spawnBullet("flyinghead", x, y, math.rad(180), 0)
+                bullet.target_x = Game.battle.arena.x
+                -- Dont remove the bullet offscreen, because we spawn it offscreen
+                bullet.remove_offscreen = false
+                wait(0.1)
+            end
+        end)
     end)
+end
+
+function Basic:onArenaEnter()
+    Game.battle.arena:setSize(270,110)
 end
 
 function Basic:update()

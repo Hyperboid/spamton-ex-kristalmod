11c3b51e7a0285d0bfdc1c2feac6cc8fccb1d4b5
hyperboid <hyperboid@nullanoid.net>
2024-10-15 16:27:30 -0400

    Finish Spamscape object

--- scripts/objects/Spamscape.lua
+++ scripts/objects/Spamscape.lua
@@ -4,17 +4,32 @@ local Spamscape, super = Class(Object)
 function Spamscape:init(x,y)
     super.init(self, x,y, SCREEN_WIDTH, SCREEN_HEIGHT)
     self.background = Sprite("backgrounds/spamscape_background")
-    self.background:addChild(Sprite("backgrounds/spamscape_background", 198, 0)).flip_x = true
+    self.background:addChild(Sprite("backgrounds/spamscape_background", 198, 0)).flip_x = false
+    self.background:addChild(Sprite("backgrounds/spamscape_background", (198*2), 0)).flip_x = false
     self.background:setScale(2)
     self.midground = Sprite("backgrounds/spamscape_midground")
-    self.midground:addChild(Sprite("backgrounds/spamscape_midground", 198, 0)).flip_x = true
+    self.midground:addChild(Sprite("backgrounds/spamscape_midground", 198, 0)).flip_x = false
+    self.midground:addChild(Sprite("backgrounds/spamscape_midground", (198*2), 0)).flip_x = false
     self.midground:setScale(2)
     self.foreground = Sprite("backgrounds/spamscape_foreground")
-    self.foreground:addChild(Sprite("backgrounds/spamscape_foreground", 198, 0)).flip_x = true
+    self.foreground:addChild(Sprite("backgrounds/spamscape_foreground", 198, 0)).flip_x = false
+    self.foreground:addChild(Sprite("backgrounds/spamscape_foreground", (198*2), 0)).flip_x = false
     self.foreground:setScale(2)
     self:addChild(self.background)
     self:addChild(self.midground)
     self:addChild(self.foreground)
+    self.speed = 100
+    self.time = 0
+end
+
+local bg_speed = 0.3
+local mg_speed = 0.7
+local fg_speed = 1.0
+function Spamscape:update()
+    self.time = self.time + (DT * self.speed)
+    self.background.x = -((bg_speed * self.time) % (198*2))
+    self.midground.x = -((mg_speed * self.time) % (198*2))
+    self.foreground.x = -((fg_speed * self.time) % (198*2))
 end
 
 return Spamscape

fb7e05471291275fb5ea72673e667d113e8809f9
THatWAGuy <176648122+THatWAGuy@users.noreply.github.com>
2024-10-15 23:00:44 -0500

    intro cutscene

---
+++ scripts/battle/cutscenes/intro.lua
@@ -0,0 +1,9 @@
+return {
+    susie_intro = function(cutscene, battler, enemy)
+
+
+        cutscene:text("[face:susie/teeth_b][voice:susie]* Kris, what the hell??\nWe're going backwards?")
+        cutscene:text("[face:susie/annoyed][voice:susie]* We need to win FAST before we run outta track!")
+
+    end
+}

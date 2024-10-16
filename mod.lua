c1e07858907a039ee54aedaa0a29d26506f9648e
hyperboid <hyperboid@nullanoid.net>
2024-10-16 00:52:01 -0400

    Make music a bit quieter

--- mod.lua
+++ mod.lua
@@ -1,3 +1,5 @@
 function Mod:init()
+    -- this is a feature kristal has and THEY DON'T TELL YOU WTF (i only know bc dark place uses it)
+    MUSIC_VOLUMES["murderbydeals"] = 0.8
     print("Loaded "..self.info.name.."!")
 end

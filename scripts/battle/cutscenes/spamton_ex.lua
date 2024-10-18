3989538a0305a7e846e4aaea7cf109c2608fa257
hyperboid <hyperboid@nullanoid.net>
2024-10-18 15:37:30 -0400

    NO MORE shall I abuse metatables

--- scripts/battle/cutscenes/spamton_ex.lua
+++ scripts/battle/cutscenes/spamton_ex.lua
@@ -1,37 +1,27 @@
 local scenes = {}
-local chara = setmetatable({},{
-    __index = function (t, k)
-        return ({
-            kris = Game.battle.party[1],
-            susi = Game.battle.party[2],
-            rals = Game.battle.party[3],
-            spam = Game.battle.enemies[1]
-        })[k]
-    end
-})
 local function simpleDialog(text)
-    return function(cutscene)
-        cutscene:battlerText(chara.spam, text)
+    return function(cutscene, spamton, kris, susie, ralsei)
+        cutscene:battlerText(spamton, text)
     end
 end
 scenes.turnbyturn = {
-    function(cutscene)
-        cutscene:battlerText(chara.spam,Dedent[==[
+    function(cutscene, spamton, kris, susie, ralsei)
+        cutscene:battlerText(spamton,Dedent[==[
             HOLY [Howitzer]!! I'M
             SO [Ex] I CAN [Almost]
             SEE PAST THE DARK!]==])
-        chara.spam.sprite:setSerious(true)
-        cutscene:battlerText(chara.spam,"[speed:0.2]...[speed:0.6] [Almost].")
-        chara.spam.sprite:setSerious(false)
-        cutscene:battlerText(chara.spam,Dedent[==[
+        spamton.sprite:setSerious(true)
+        cutscene:battlerText(spamton,"[speed:0.2]...[speed:0.6] [Almost].")
+        spamton.sprite:setSerious(false)
+        cutscene:battlerText(spamton,Dedent[==[
             KRIS! MY [Little sponge]
             BE A [[BIG SHOT!!]] AND
             GIMME THAT [Soul.]
             YOU GOT!!!]==])
     end,
 }
-function scenes.fallback(cutscene)
-    cutscene:battlerText(chara.spam, {
+function scenes.fallback(cutscene, spamton, kris, susie, ralsei)
+    cutscene:battlerText(spamton, {
         Utils.pick({"KRIS! YOU [Little Sponge]", "SUSIE! YOU [Chalk Eater]", "RALSEI! YOU [Scringly Dingly]!"}),
     })
 end
@@ -39,11 +29,14 @@ end
 function scenes.master(cutscene)
 	local enc = Game.battle.encounter
 	local spamton = Game.battle.enemies[1]
+    local kris = Game.battle.party[1]
+    local susie = Game.battle.party[2]
+    local ralsei = Game.battle.party[3]
 	-- cutscene:battlerText(spamton, {"[voice:spamton]I HAVE [Becomed] OMEGA", "NOW YOU [Canned] HURT ME [Jack]"}, spamfiguration)
 	if scenes.turnbyturn[Game.battle.turn_count] then
-		scenes.turnbyturn[Game.battle.turn_count](cutscene)
+		scenes.turnbyturn[Game.battle.turn_count](cutscene, spamton, kris, susie, ralsei)
     else
-        scenes.fallback(cutscene)
+        scenes.fallback(cutscene, spamton, kris, susie, ralsei)
 	end
 end
 return scenes

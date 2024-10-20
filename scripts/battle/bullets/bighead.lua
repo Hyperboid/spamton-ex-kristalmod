71229d677e953c71c7c2bf319464c3eb19416324
hyperboid <hyperboid@nullanoid.net>
2024-10-20 14:28:42 -0400

    Make big head shoot pipis

--- scripts/battle/bullets/bighead.lua
+++ scripts/battle/bullets/bighead.lua
@@ -3,7 +3,7 @@ local BigHead, super = Class(VisualBullet)
 
 function BigHead:init(x,y,variation)
     super.init(self,x,y, "bullets/bighead/turn")
-    self.sprite:setAnimation{"bullets/bighead/turn", 0.2, true}
+    self.sprite:setAnimation{"bullets/bighead/turn", 0.4, false}
     self:setScale(2.6)
 end
 
@@ -23,6 +23,31 @@ function BigHead:onAdd(parent)
         })
         self:setLayer(400)
         wait(0.5)
+        self.sprite:setAnimation{"bullets/bighead/flip", 0.1, true}
+        wait(0.1)
+        for i = 1, 3 do
+            self:shake(3 * i, 0, 0.5, 0.1)
+            wait(0.4)
+        end
+        wait(0.2)
+        self.sprite:setAnimation{"bullets/bighead/open", 0.1, false}
+        wait(0.5)
+        self.sprite:setAnimation{"bullets/bighead/shoot", 0.1, true}
+        for i = -1, 1 do
+            ---@type Pipis
+            local pipis = self.wave:spawnBullet("pipis", self.x, self.y)
+            pipis:setPhysics({
+                gravity = 1.0,
+                speed = 0,
+                speed_x = i * 3,
+                speed_y = -10,
+                gravity_direction = math.rad(90),
+                direction = 0,
+                friction = 0,
+                match_rotation = false,
+                spin = 0
+            })
+        end
     end)
 end
 

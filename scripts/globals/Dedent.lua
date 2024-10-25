8828c63ff50fb67ca58abf9d1e637229b6953238
hyperboid <hyperboid@nullanoid.net>
2024-10-25 17:18:32 -0400

    ACTUALLY Make Dedent work better

--- scripts/globals/Dedent.lua
+++ scripts/globals/Dedent.lua
@@ -5,8 +5,8 @@ function Dedent(str)
     local lines = Utils.split(str, "\n", false)
     local shortest_indentation = math.huge
     for _, str in ipairs(lines) do
-        if str == "" then break end
-        if str == string.rep(" ", #str) then break end
+        if str == "" then goto continue end
+        if str == string.rep(" ", #str) then goto continue end
         local this_indent = 1
         for i=1, #str do
             local char = str:sub(i,i)
@@ -14,6 +14,7 @@ function Dedent(str)
             this_indent = this_indent + 1
         end
         shortest_indentation = math.min(shortest_indentation, this_indent)
+        ::continue::
     end
     for i=1,#lines do
         lines[i] = lines[i]:sub(shortest_indentation)
